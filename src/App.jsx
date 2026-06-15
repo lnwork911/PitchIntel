5b7c3117a88739a9c5714079181a9e64fcc0203b
lnwork911 <lnguyenwork911@gmail.com>
2026-06-14 23:08:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -223,22 +223,6 @@ return (
      
 
           
-          <div className="card">          
-            <h3>
-              📰 Recent Stories
-            </h3>
-          
-            {articles.map(article => (
-          
-              <div
-                key={article.id}
-                className="archive-item"
-              >
-                {article.title}
-              </div>          
-            ))}
-          </div> 
-           
           
           <div className="card">
             <h3>📈 Momentum Rankings</h3>
