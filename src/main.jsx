2e8af39c92046931f17c468f44bac3052a0a9078
lnwork911 <lnguyenwork911@gmail.com>
2026-06-24 02:57:41 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import StoryPage from "./StoryPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/story/:id" element={<StoryPage />} />
+    </Routes>
+  </BrowserRouter>
 );
